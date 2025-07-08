18d27295ae218b373b6e0a11fac70510dc17faf7
Anna <anna.ly@convergint.com>
2025-07-08 11:06:23 +1000

    Feat: Revised functionality to make NFC passes + Checks for existing Pass classes
    Co-authored-by: bmatheson3 <bmatheson3@users.noreply.github.com>

--- app/google_wallet.py
+++ app/google_wallet.py
@@ -9,6 +9,15 @@ from google.auth import jwt, crypt
 
 
 class GenericPass:
+    """
+    Class that defines the a Generic Pass using the Google Wallet API.
+    
+    Initialisers:
+        key_file_path: Google ADC (Application Default Credentials)
+        issuer_id: Unique ID provided for using Google Wallet API
+        class_id: Customisable class name for pass
+        auth(): Checks user's credentials to authorise
+    """
     
     load_dotenv()
     
@@ -16,7 +25,7 @@ class GenericPass:
     def __init__(self):
         self.key_file_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
         self.issuerId = os.getenv("ISSUER_ID")
-        self.classId = self.issuerId + '.flask_noAI'
+        self.classId = self.issuerId + '.flask_noAI_nfc_try'
         self.auth()
         print(self.issuerId)
         print(self.classId)
@@ -35,10 +44,15 @@ class GenericPass:
 
     # Create class
     def create_class(self):
+        """
+        Method to create a base class from which the Google pass objects can instantiate
+        """
 
         # Defining class structure (json)
+        # This was taken from Google's developers tutorial (in js) and was adapted for python
         generic_class = {
             'id': self.classId,
+            'enableSmartTap': True,
             'classTemplateInfo': {
                 'cardTemplateOverride': {
                     'cardRowTemplateInfos': [
@@ -141,31 +155,36 @@ class GenericPass:
         # Check if the class already exists
         try:
             self.client.genericclass().get(resourceId=self.classId).execute()
+        # If it does not, create it
         except HttpError as e:
+            # Checking for any unexpected errors (we expect a 404 in the case that the class does not exist)
             if e.status_code != 404:
-                # Something else went wrong...
                 print(e.error_details)
                 return self.classId
         else:
-            print(self.classId + "already exists!")
+            print(self.classId + " already exists!")
             return self.classId
 
-        # If it does not, create it
-        response = self.client.genericclass().insert(body=generic_class).execute()
-        print('Class insert response')
-        print(response)
+        # Creating the class with the generic class json
+        self.client.genericclass().insert(body=generic_class).execute()
+        print("Created class with ID: " + self.classId)
         
         return self.classId
 
 
     # Create pass object
     def create_pass_object(self):
+        '''
+        Method to generate a URL that allows a unique nfc pass (with a unique ID) to be added to the Google wallet
+        '''
         object_suffix = str(uuid.uuid4()).replace('[^\\w.-]', '_')
         object_id = self.issuerId + "." + object_suffix
         
+        # This was taken from Google's developers tutorial (in js) and was adapted for python
         genericObject = {
             'id': object_id,
             'classId': self.classId,
+            'smartTapRedemptionValue': object_id,
             'genericType': 'GENERIC_TYPE_UNSPECIFIED',
             'hexBackgroundColor': '#4285f4',
             'logo': {
@@ -188,7 +207,7 @@ class GenericPass:
             'header': {
             'defaultValue': {
                 'language': 'en',
-                'value': 'Alex McJacobs'
+                'value': 'Alex McJacobs NFC'
             }
             },
             'barcode': {
@@ -214,15 +233,13 @@ class GenericPass:
             ]
         }
         
-        # Create the JWT claims
+        # Create the JWT claims 
         claims = {
             'iss': self.credentials.service_account_email,
             'aud': 'google',
             'origins': ['www.example.com'],
             'typ': 'savetowallet',
             'payload': {
-                # The listed classes and objects will be created
-                # 'genericClasses': [],
                 'genericObjects': [genericObject]
             }
         }
@@ -232,6 +249,7 @@ class GenericPass:
         token = jwt.encode(signer, claims).decode('utf-8')
 
         print('Add to Google Wallet link')
+        # long link for now, need to shorten
         print(f'https://pay.google.com/gp/v/save/{token}')
         
         return f'https://pay.google.com/gp/v/save/{token}'
