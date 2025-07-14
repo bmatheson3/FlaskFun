5ca03ec33b9498ed07b93e93ee42cdcd75af91a2
Blake Matheson <blake.matheson@convergint.com>
2025-07-14 16:42:26 +1000

    Progress made on landing page using svelte
    Co-authored-by: aavly <aavly@users.noreply.github.com>

--- frontend/Flask-NoAI/src/routes/+page.svelte
+++ frontend/Flask-NoAI/src/routes/+page.svelte
@@ -1,1 +1,25 @@
-<h1>Welcome to SECOND PAGE</h1>
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+  import { goto } from '$app/navigation';
+  import logo from '$lib/assets/splash.avif'
+
+</script>
+
+
+<div class="mx-auto flex justify-center">
+        
+        <img class="object-cover" alt="The project logo" src={logo} />
+
+		<div class="">
+			<h1 class="text-6xl absolute top-2/5 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">Welcome to Convergint</h1>
+		</div>
+
+        
+        <h2 class="text-4xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">Manage your client's passes</h2>
+		<div class="absolute top-3/5 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+			<Button class="px-10 py-7 rounded-xl" onclick={() => goto('/users')}>
+				<p class="text-xl">Login</p>
+			</Button>
+		</div>
+</div>
+
---
+++ frontend/Flask-NoAI/src/routes/users/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.js";
+</script>
+
+<h1>Welcome to the USERS page</h1>
+
+
+
